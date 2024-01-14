d63c23ef7c8d6d7275b1b16238703fdb21b8575b
yoheikikuta <diracdiego@gmail.com>
2024-01-14 16:25:00 +0900

    Update SITE_TITLE and SITE_DESCRIPTION constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '原理的には可能';
+export const SITE_DESCRIPTION = 'データ分析界隈の人のブログ、もとい雑記帳';
