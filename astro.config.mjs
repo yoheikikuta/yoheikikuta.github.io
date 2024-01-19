1b75c6bb27e4d1b62303225f332517ceee8c3b47
yoheikikuta <diracdiego@gmail.com>
2024-01-19 21:45:26 +0900

    Add KateX into project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,15 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	}
 });
