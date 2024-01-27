36266367faf92c58097313ae20a337a6689698f7
yoheikikuta <diracdiego@gmail.com>
2024-01-27 21:24:19 +0900

    Restore KaTeX

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,15 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-// import remarkMath from 'remark-math';
-// import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://yoheikikuta.github.io',
 	integrations: [mdx(), sitemap()],
-	// markdown: {
-	// 	remarkPlugins: [remarkMath],
-	// 	rehypePlugins: [rehypeKatex],
-	// }
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	}
 });
